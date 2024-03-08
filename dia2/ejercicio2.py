cf69e7a53aad1a4698c8621996d8419c3bb61ae0
Israel Palma <ipalm001@gmail.com>
2024-03-08 18:29:17 -0300

    string.format

---
+++ dia2/ejercicio2.py
@@ -0,0 +1,20 @@
+#la separacion de los float es un punto (.)
+#la division entre integers, el resultado es un float
+
+print(4/2) #2.0
+print(5/2) #2.5
+print(5/3.5)#1.4285714285714286
+print(2.4*4)#9.6
+
+nombre = "Mijail"
+año = "2024"
+print(3*nombre)#MijailMijailMijail
+print(año*2)#20242024
+#print(nombre/2)#no se puede dividir un string
+
+# Interpolacion de cadenas(otra forma de imprimir) print(f" {nombre_variable}")
+mes = 3
+dia = 7
+
+print(f"Hola {nombre}, El año es {año} del mes {mes} y el dia {dia} ")
+
