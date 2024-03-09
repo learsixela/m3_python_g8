7acff2b75ffb3b56578e507513a2d8daf91c50b8
Israel Palma <ipalm001@gmail.com>
2024-03-08 21:57:46 -0300

    contando calorias

--- dia2/librerias.py
+++ dia2/librerias.py
@@ -12,4 +12,9 @@ print(5%2)  # 1, modulo:el resto de la division
 print(os.getcwd()) #
 print(math.sqrt(9))#
 #print(ceil(3.1416)) #importacion especifica de una libreria
-print(math.ceil(3.1416))
+print(math.ceil(3.1416))#ceil aproxima al entero superior
+
+#pip -V  -> (win)para saber la version
+#pip3 -V -> (mac)para saber la version
+#python3 -m pip install --upgrade pip -> actualizar pip
+
